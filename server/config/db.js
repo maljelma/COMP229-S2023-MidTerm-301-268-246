abf21c39199f0a46db908856ebca2aa77cb5b91a
maljelma <133683358+maljelma@users.noreply.github.com>
2023-06-21 16:34:18 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  
+  // MongoDB Atlas deployment ->
+  "URI": "mongodb+srv://maljelma:tNjIjt7ZM9RS0Y7y@mid-term.bcktolq.mongodb.net/?retryWrites=true&w=majority"
+};
